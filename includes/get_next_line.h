08a41e63096cb775e2e420b15dfd243e808ac5d1
pnoronha <pnoronha@student.42lisboa.com>
2022-03-08 16:29:34 +0000

    Protecting BUFFER_SIZE macro definition

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pnoronha <pnoronha@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: pnoronha <pnoronha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 10:03:21 by pnoronha          #+#    #+#             */
-/*   Updated: 2022/03/06 15:53:46 by pnoronha         ###   ########.fr       */
+/*   Updated: 2022/03/08 16:27:52 by pnoronha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 
 # include "libft.h"
 
-# define BUFFER_SIZE 42
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 int		get_next_line(int fd, char **line);
 
