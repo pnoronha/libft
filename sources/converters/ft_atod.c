4d42a4f35c9046683e136a0b9da757ea609c8ef7
pnoronha <pnoronha@student.42lisboa.com>
2022-05-02 23:13:17 +0100

    fixing norm

--- sources/converters/ft_atod.c
+++ sources/converters/ft_atod.c
@@ -6,50 +6,50 @@
 /*   By: pnoronha <pnoronha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 15:23:43 by pnoronha          #+#    #+#             */
-/*   Updated: 2022/04/20 16:08:01 by pnoronha         ###   ########.fr       */
+/*   Updated: 2022/05/02 22:59:40 by pnoronha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-double	ft_atod(const char *nptr)
+static double	converter(const char *nptr)
 {
-	int			i;
-	int			sign;
 	int			dot;
 	double		devider;
 	double		value;
 
-	i = 0;
-	while (ft_isspace((int)nptr[i]))
-		i++;
-	sign = 1;
-	if (nptr[i] == '-' || nptr[i] == '+')
-	{
-		if (nptr[i] == '-')
-			sign = -1;
-		i++;
-	}
 	devider = 10;
 	dot = 0;
 	value = 0;
-	while (ft_isdigit((int)nptr[i]) || (nptr[i] == '.' && dot != 1))
+	while (ft_isdigit((int)*nptr) || (*nptr == '.' && dot != 1))
 	{
-		if (nptr[i] == '.')
+		if (*nptr == '.' && nptr++)
 		{
 			dot++;
-			i++;
 			continue ;
 		}
 		if (dot == 1)
 		{
-			value += (double)(nptr[i] - '0') / devider;
+			value += (double)(*nptr - '0') / devider;
 			devider = devider * 10;
 		}
 		else
-			value = value * 10 + (nptr[i] - '0');
-		i++;
+			value = value * 10 + (*nptr - '0');
+		nptr++;
 	}
-	return ((double)value * sign);
+	return (value);
+}
+
+double	ft_atod(const char *nptr)
+{
+	int			sign;
+	double		value;
+
+	while (ft_isspace((int)*nptr))
+		nptr++;
+	sign = 1;
+	if (*nptr == '-' || *nptr == '+')
+		if (*nptr == '-' && nptr++)
+			sign = -1;
+	return ((double)(converter(nptr) * sign));
 }
